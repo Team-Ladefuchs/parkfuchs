f04e98963f08bb3433f29ad5470739e89dce8a5a
Dominic Wrege <dominic.wrege001@stud.fh-dortmund.de>
2022-12-21 18:27:41 +0100

    LandingBox allign icons

--- components/LandingBox.tsx
+++ components/LandingBox.tsx
@@ -13,34 +13,34 @@ export default function LandingBox({
 }: Properties): JSX.Element {
 	return (
 		<section
-			className={`bg-green rounded-lg p-6 h-fit opacity-75 grid grid-cols-[28px_1fr] gap-2 max-md:gap-4 max-md:p-5 ${className}`}
+			className={`bg-green rounded-lg p-6 h-fit opacity-75 grid grid-cols-[28px_1fr] gap-2 max-md:gap-3 max-md:p-5 ${className}`}
 		>
 			<FontAwesomeIcon
 				icon={faTreeCity}
-				className="w-5 h-5 max-md:h-7 max-md:w-7 mt-[2px]"
+				className="w-5 h-5 max-md:h-7 max-md:w-7 mt-[2px] ml-[-3px]"
 			/>
 			<p>
 				Für E-Autos gibt’s Privilegien in vielen Städten. Aber welche
 				und wo?<br></br>
-				Community-Power: Füttere den Fuchs mit Infos aus deiner Stadt.
+				Füttere den Fuchs mit Infos aus deiner Stadt.
 			</p>
 			<FontAwesomeIcon
 				icon={faSlack}
-				className="w-5 h-5 mt-[2px] max-md:h-6 max-md:w-6"
+				className="w-5 h-5 max-md:mt-[-3px] max-md:h-6 max-md:w-6"
 			/>
 			<p className="lg:ml-[-2px] max-md:block">
 				Feedback und Ideen:
 				<Link
 					href="https://parkfuchs.app/slack"
 					target="_blank"
-					className="ml-1 max-md:ml-0 underline"
+					className="ml-2 underline"
 				>
 					hier entlang!
 				</Link>
 			</p>
 			<FontAwesomeIcon
 				icon={faMugHot}
-				className="w-[22px] h-[22px]  ml-1 mt-[-5px] md:hidden"
+				className="w-[22px] h-[22px] ml-1 mt-[-1px] md:hidden"
 			/>
 			<Link className="leading-5 underline md:hidden" href={bunqLink}>
 				Danke sagen.
