78c949013128f4427cce46b26ffee42ab49cd962
Dominic Wrege <dominic.wrege001@stud.fh-dortmund.de>
2022-12-26 15:10:09 +0100

    TGH banner height on none standalone

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,15 +4,39 @@ import banner400eu from "../public/banner/400eu_Footer_1400x500.png";
 
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { run } from "node:test";
 
 function getBannerImg(): StaticImageData {
 	const date = new Date().getFullYear();
 	return date === 2022 ? banner400eu : banner250eu;
 }
 
+function runningStandalone(): boolean {
+	const isInWebAppiOS = window.navigator["standalone"] === true;
+	if (isInWebAppiOS) {
+		return true;
+	}
+
+	return window.matchMedia("(display-mode: standalone)").matches;
+}
+
 export default function Banner(): JSX.Element {
+	const [mobileWrapperHeight, setMobileWrapperHeight] =
+		useState("max-md:h-[4.5rem]");
+
+	useEffect(() => {
+		if (!window) {
+			return;
+		}
+		if (!runningStandalone()) {
+			setMobileWrapperHeight("");
+		}
+	}, []);
 	return (
-		<div className="fixed left-0 right-0 bottom-0 bg-green z-10 w-full h-[3.85rem] banner-shadow max-md:h-[4.5rem] ">
+		<div
+			className={`fixed left-0 right-0 bottom-0 bg-green z-10 w-full h-[3.85rem] banner-shadow ${mobileWrapperHeight}`}
+		>
 			<Link
 				href="https://xn--geld-fr-eauto-1ob.de/ref/Parkfuchs"
 				target="_blank"
