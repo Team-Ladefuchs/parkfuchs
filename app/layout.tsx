0c068da7584eb2ae992061b0c04dbb729a3cc41d
Dominic Wrege <dominic.wrege001@stud.fh-dortmund.de>
2023-05-07 10:39:39 +0200

    ci config server side rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,71 @@ export const metadata: Metadata = {
 	themeColor: "#D1D9C0",
 	viewport: "width=device-width, initial-scale=1.0",
 	manifest: "/manifest.json",
+	icons: [
+		{
+			rel: "apple-touch-startup-image",
+			href: "/splashscreens/apple-splash-2048-2732.png",
+			media: "(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",
+			url,
+		},
+		{
+			rel: "apple-touch-startup-image",
+			href: "/splashscreens/apple-splash-1668-2388.png",
+			media: "(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",
+			url,
+		},
+		{
+			rel: "apple-touch-startup-image",
+			href: "/splashscreens/apple-splash-1536-2048.png",
+			media: "(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",
+			url,
+		},
+		{
+			rel: "apple-touch-startup-image",
+			href: "/splashscreens/apple-splash-1668-2224.png",
+			media: "(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",
+			url,
+		},
+		{
+			rel: "apple-touch-startup-image",
+			href: "/splashscreens/apple-splash-1620-2160.png",
+			media: "(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",
+			url,
+		},
+		{
+			rel: "apple-touch-startup-image",
+			href: "/splashscreens/apple-splash-1290-2796.png",
+			media: "(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",
+			url,
+		},
+		{
+			href: "/icons/icon-48x48.png",
+			rel: "icon",
+			type: "image/png",
+			sizes: "48x48",
+			url,
+		},
+		{
+			href: "/icons/icon-72x72.png",
+			rel: "icon",
+			type: "image/png",
+			sizes: "72x72",
+			url,
+		},
+
+		{
+			rel: "apple-touch-icon",
+			sizes: "144x144",
+			href: "/icons/icon-144x144.png",
+			url,
+		},
+		{
+			rel: "icon",
+			type: "image/x-icon",
+			href: "favicon.ico",
+			url,
+		},
+	],
 	openGraph: {
 		type: "website",
 		locale: "de_DE",
@@ -22,15 +87,15 @@ export const metadata: Metadata = {
 		description: "Parkfuchs - Schlau elektrisch parken",
 		images: {
 			type: "image/jpeg",
-			url: "/parkfuchs-opengraph.jpg",
+			url: `${url}/parkfuchs-opengraph.jpg`,
 		},
 	},
-	// twitter: {
-	// 	title: "Parkfuchs",
-	// 	description: "Parkfuchs - Schlau elektrisch parken",
-	// 	images: "/parkfuchs-opengraph.jpg",
-	// 	site: url,
-	// },
+	twitter: {
+		title: "Parkfuchs",
+		description: "Parkfuchs - Schlau elektrisch parken",
+		images: `${url}/parkfuchs-opengraph.jpg`,
+		site: url,
+	},
 };
 
 export default function RootLayout({ children }) {
