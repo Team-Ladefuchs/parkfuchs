d04d12f3307e366a308ae1d9c5a106d6091b7f0c
Dominic Wrege <dominic.wrege001@stud.fh-dortmund.de>
2023-06-04 18:53:37 +0200

    update deps

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import fuchsLog from "../public/parkfuchs.svg";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function () {
+export default function NotFound() {
 	return (
 		<div className="h-full grid justify-items-center mt-28 gap-2">
 			<Image src={fuchsLog} alt="Parkfuchs logo" height={100} />
