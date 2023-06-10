b55717ded5153e511a5c059629340f55e19edc72
Dominic Wrege <dominic.wrege001@stud.fh-dortmund.de>
2023-06-10 09:50:19 +0200

    bug search input

--- context/appContext.tsx
+++ context/appContext.tsx
@@ -4,15 +4,11 @@ import { InboxCity } from "../db/types";
 interface AppContextType {
 	editCity: InboxCity | null;
 	setEditCity: (city: InboxCity | null) => void;
-	searchQuery: string;
-	setSearchQuery: (value: string) => void;
 }
 
 export const AppContext = createContext<AppContextType>({
 	editCity: null,
 	setEditCity: () => {},
-	searchQuery: "",
-	setSearchQuery: () => {},
 });
 
 interface AppContextProviderProps {
@@ -28,19 +24,11 @@ export const AppContextProvider: React.FC<AppContextProviderProps> = ({
 		setCity(updateCity);
 	};
 
-	const [searchQuery, setQuery] = useState<string>("");
-
-	const setSearchQuery = (value: string) => {
-		setQuery(value);
-	};
-
 	return (
 		<AppContext.Provider
 			value={{
 				editCity: selectedCity,
 				setEditCity: setSelectedCity,
-				searchQuery,
-				setSearchQuery,
 			}}
 		>
 			{children}
