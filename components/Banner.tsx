c3d1f39aa1f3d4038fabd3e70d84f246d2f97f9a
Dominic Wrege <dominic.wrege001@stud.fh-dortmund.de>
2023-08-14 16:03:23 +0200

    replace thg banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,4 +1,4 @@
-import banner250eu from "../public/banner/250eu_Footer_1400x500.png";
+import bannerThg from "../public/banner/banner_parkfuchs_thg.png";
 
 import Image from "next/image";
 import Link from "next/link";
@@ -12,7 +12,7 @@ export default function Banner({ link }: Props): JSX.Element {
 		<div className="fixed left-0 right-0 bottom-0 bg-green z-10 w-full h-[4.3rem] banner-shadow md:h-[5.0rem]">
 			<Link prefetch={false} href={link} target="_blank">
 				<Image
-					src={banner250eu}
+					src={bannerThg}
 					height={120}
 					alt={"thg Banner"}
 					className="mx-auto relative bottom-3 max-sm:bottom-[12px]"
