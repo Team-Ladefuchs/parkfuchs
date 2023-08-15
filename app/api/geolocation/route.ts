c43bcb912f12ad85708267ef40506b0dff13e060
Dominic Wrege <dominic.wrege001@stud.fh-dortmund.de>
2023-08-15 20:03:18 +0200

    fix build

--- app/api/geolocation/route.ts
+++ app/api/geolocation/route.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
