030d719e990711727ac507257184e3d8e7cbb8e1
Dominic Wrege <dominic.wrege001@stud.fh-dortmund.de>
2023-09-22 17:33:51 +0200

    api getCitiesWithPrivileges and upgrade to next 13.5.2

--- app/api/latestCity/route.ts
+++ app/api/latestCity/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getNewestEnabledInboxCities } from "../../../db/city";
+import { getEnabledInboxCities } from "../../../db/city";
 
 export async function GET(_request: NextRequest) {
-	return NextResponse.json(await getNewestEnabledInboxCities());
+	return NextResponse.json(await getEnabledInboxCities());
 }
