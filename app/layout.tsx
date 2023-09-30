9446df8384bb49c4b76d0f74208de8f1b2f99cec
Dominic Wrege <dominic.wrege001@stud.fh-dortmund.de>
2023-10-01 00:15:35 +0200

    update thg banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const url = "https://parkfuchs.app";
 export const metadata: Metadata = {
 	title: "Parkfuchs",
 	description: "Schlau elektrisch parken",
-	themeColor: "#ece9e1",
+	themeColor: "#D1D9C0B8",
 	viewport: "width=device-width, initial-scale=1.0",
 	manifest: "/manifest.json",
 	appleWebApp: {
