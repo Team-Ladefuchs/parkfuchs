32ef4c28ae2c613c6044bbfb581fa2f04b94cb89
Dominic <email@dwrege.de>
2023-12-30 17:31:36 +0100

    build nix module

--- nix/default.nix
+++ nix/default.nix
@@ -1,6 +1,7 @@
-{ self, nixpkgs }: { pkgs, lib, config, ... }:
+{ self, nixpkgs }: { pkgs, lib, config, unstable, ... }:
 let
   cfg = config.services.parkfuchs;
+  stateDir = "pocktetbase-db";
 in
 {
   options = with lib; {
@@ -10,32 +11,67 @@ in
         type = types.str;
         default = "0.0.0.0";
         description = ''
-          Addr to run shutdown-thing on.
+          Addr to run parkfuchs on.
         '';
       };
+
       port = lib.mkOption {
         type = types.port;
         default = 3000;
         description = ''
-          Port to run shutdown-thing on.
+          Port to run parkfuchs on.
         '';
       };
+
+      pocketBaseHost = lib.mkOption {
+        type = types.str;
+        default = "http://localhost:8090";
+        description = ''
+		   Pokcetbase Host URL
+        '';
+      };
+
     };
   };
+  
   config = lib.mkIf (cfg.enable) {
+    users.users.parkfuchs = {
+      isSystemUser = true;
+      group = "parkfuchs";
+    };
+    users.groups.parkfuchs = { };
+
+    systemd.services.pocketbase = {
+      enable = true;
+      description = "Pocketbase: Open Source backend";
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "simple";
+        User = "parkfuchs";
+		LimitNOFILE = "4096";
+		Restart = "always";
+		RestartSec = "5s";
+        Group = "parkfuchs";
+		StateDirectory = stateDir;
+		# Starts the web server (default to 127.0.0.1:8090 if no domain is specified)
+        ExecStart = "${pkgs.pocketbase}/bin/pocketbase serve --dir=/var/lib/${stateDir}";
+      };
+    };
+
     systemd.services.parkfuchs = {
       enable = true;
       description = "Parkfuchs Web App";
-      wantedBy = [ "multi-user.target" ];
+      wantedBy = [ "multi-user.target" "pocketbase.service" ];
       environment = {
         ADDR = cfg.addr;
         PORT = toString cfg.port;
+		DB_HOST = cfg.pocketBaseHost;
       };
       serviceConfig = {
         Type = "simple";
         User = "parkfuchs";
         Group = "parkfuchs";
-        ExecStart = "${pkgs.nodejs_20}/bin/node ${self.packages.${pkgs.system}.parkfuchs}/server.js";
+        ExecStart = "${pkgs.nodejs_20}/bin/node ${self.packages.${pkgs.system}.parkfuchs}/server.js ";
       };
     };
   };
