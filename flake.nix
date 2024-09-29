969f9c70c1e77ef7ad05fcc886940d0b44ca0054
Dominic <email@dwrege.de>
2024-09-29 11:24:38 +0200

    fix nix build

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
         npmPackFlags = [ "--ignore-scripts" "--legacy-peer-deps" ];
         pname = "parkfuchs";
         version = (builtins.fromJSON (builtins.readFile ./package.json)).version;
-        npmDepsHash = "sha256-QM18fvR0rXu8v+YgYj71iDhgpLit65IoKemeEVvLxUU=";
+        npmDepsHash = "sha256-AtKB9Gwp+Xr5vgdoBw2Z/6ENrX94NjnY17jZeUgSa74=";
         installPhase = ''
           			runHook preInstall
           			mkdir -p $out/.next
