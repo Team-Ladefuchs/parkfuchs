5ab62778c89336d0655881e42cd240049bcf90b0
Dominic <email@dwrege.de>
2024-11-10 19:53:48 +0100

    fix banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { JSX } from "react";
-import Image from "next/image";
 
 interface Props {
 	link: string;
@@ -10,7 +9,7 @@ export default function Banner({ link }: Props): JSX.Element {
 	return (
 		<div className="fixed left-0 right-0 bottom-0 bg-primaryGreen z-10 w-full h-[4.3rem] banner-shadow md:h-[5.0rem]">
 			<Link prefetch={false} href={link} target="_blank">
-				<Image
+				<img
 					src="https://parkfuchs.app/images/banner/banner_parkfuchs_thg.png"
 					alt={"thg Banner"}
 					className="mx-auto relative bottom-3 max-sm:bottom-[12px] h-[120px]"
