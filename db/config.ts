857fc29559f91fecbec77a2695c6545d171f50bb
Dominic <email@dwrege.de>
2025-03-23 17:39:34 +0100

    fix  backend

--- db/config.ts
+++ db/config.ts
@@ -1,9 +1,10 @@
-import { pocketBase } from "./city";
+import { pocketBaseInstance } from "./city";
 import { Config } from "./types";
 
 export async function getTGHLink(): Promise<string> {
 	const defaultTGH = "https://xn--geld-fr-eauto-1ob.de/ref/Parkfuchs45";
 	try {
+		const pocketBase = await pocketBaseInstance();
 		const result = await pocketBase
 			.collection("config")
 			.getFirstListItem<Config>("field = 'tghLink'", {});
